b1353f4ca533083784ca84cc0e27e5c45ae7915a
Rodrigo Nikolof <rodrigo.nikolof@outlook.com>
2023-04-07 12:58:29 +1000

    Added toggle between sign up and login in Login page

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { ToggleButton, ToggleButtonGroup } from "@mui/material";
 import LoginForm from '../components/LoginForm'
 import SignUpForm from "../components/SignUpForm";
 
@@ -7,11 +8,31 @@ import SignUpForm from "../components/SignUpForm";
 
 function Login({onLogin}){
 
+const [signUp, setSignUp] = useState(true)
 
+const handleChange = (event, selection)=>{
+     setSignUp(selection)
+}
+
+console.log(signUp)
     return (
         <div>
-            <LoginForm/>
-            <SignUpForm/>
+            <ToggleButtonGroup
+            value={signUp}
+            exclusive
+            onChange={handleChange}
+            >
+                <ToggleButton value={true}>
+                    Sign Up
+                </ToggleButton>
+                <ToggleButton value={false}>
+                    Login
+                </ToggleButton>
+            </ToggleButtonGroup>
+
+            {signUp? <SignUpForm/>: <LoginForm/>}
+            
+            
         </div>
     )
 
